9421a574a3edf3a353fc828c8217f565b78ba781
ares2347 <88496216+ares2347@users.noreply.github.com>
2022-06-10 20:55:13 +0700

    Angular update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AppHeader} from "./header/header.component";
+import {AppNavbar} from "./navbar/navbar.component";
+import {AppFooter} from "./footer/footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeader,
+    AppNavbar,
+    AppFooter
   ],
   imports: [
     BrowserModule
